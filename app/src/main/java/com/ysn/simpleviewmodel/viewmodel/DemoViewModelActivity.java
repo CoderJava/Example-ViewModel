a0c437a753d9b96c75889a9c2e0f5fd81e2c306e
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-06-01 11:48:36 +0700

    Update demo ViewModel to new package

--- app/src/main/java/com/ysn/simpleviewmodel/viewmodel/DemoViewModelActivity.java
+++ app/src/main/java/com/ysn/simpleviewmodel/viewmodel/DemoViewModelActivity.java
@@ -1,11 +1,11 @@
 /*
- * Created by YSN Studio on 6/1/18 11:18 AM
+ * Created by YSN Studio on 6/1/18 11:48 AM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 6/1/18 11:16 AM
+ * Last modified 6/1/18 11:24 AM
  */
 
-package com.ysn.simpleviewmodel;
+package com.ysn.simpleviewmodel.viewmodel;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
@@ -13,13 +13,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.ysn.simpleviewmodel.R;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class DemoViewModelActivity extends AppCompatActivity {
 
-    @BindView(R.id.text_view_count_activity_main)
+    @BindView(R.id.text_view_count_activity_demo_view_model)
     TextView textViewCountActivityMain;
 
     private ClickCounterViewModel clickCounterViewModel;
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_demo_view_model);
         ButterKnife.bind(this);
 
         // The factory and its dependencies instead should be injected with DI framework like Dagger
@@ -38,16 +40,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @OnClick({
-            R.id.button_increment_activity_main,
-            R.id.button_decrement_activity_main
+            R.id.button_increment_activity_demo_view_model,
+            R.id.button_decrement_activity_demo_view_model
     })
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.button_increment_activity_main:
+            case R.id.button_increment_activity_demo_view_model:
                 clickCounterViewModel.setCount(clickCounterViewModel.getCount() + 1);
                 displayClickCount();
                 break;
-            case R.id.button_decrement_activity_main:
+            case R.id.button_decrement_activity_demo_view_model:
                 clickCounterViewModel.setCount(clickCounterViewModel.getCount() - 1);
                 displayClickCount();
                 break;
